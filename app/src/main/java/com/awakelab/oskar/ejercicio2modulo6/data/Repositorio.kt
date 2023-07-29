d0cb0628ae71bb388f6d6bd93ef2224986e0afc1
OskarUno <oorellana80@gmail.com>
2023-07-28 21:19:57 -0400

    Ejercicio 2. Hito 1.1. Se crea data class, interface, ddbb y repositorio

---
+++ app/src/main/java/com/awakelab/oskar/ejercicio2modulo6/data/Repositorio.kt
@@ -0,0 +1,18 @@
+package com.awakelab.oskar.ejercicio2modulo6.data
+
+import androidx.lifecycle.LiveData
+import com.awakelab.oskar.ejercicio2modulo6.data.local.Item
+import com.awakelab.oskar.ejercicio2modulo6.data.local.ItemDao
+
+class Repositorio(private val itemDao: ItemDao) {
+
+    //Recibe un item desde el view
+    suspend fun insertTask(item: Item) {
+        itemDao.insetarItem(item)
+    }
+
+    fun getItem(): LiveData<List<Item>> {
+        return itemDao.getAllItems()
+    }
+
+}
