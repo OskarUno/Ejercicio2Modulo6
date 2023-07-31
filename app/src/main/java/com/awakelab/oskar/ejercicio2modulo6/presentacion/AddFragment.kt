cd8456edfda283248ec4f7d01d41b5b447dcffe1
OskarUno <oorellana80@gmail.com>
2023-07-31 19:56:33 -0400

    Ejercicio 2. Se agrega en fragmentoAdd guardan informacion en la bbdd

--- app/src/main/java/com/awakelab/oskar/ejercicio2modulo6/presentacion/AddFragment.kt
+++ app/src/main/java/com/awakelab/oskar/ejercicio2modulo6/presentacion/AddFragment.kt
@@ -5,16 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.awakelab.oskar.ejercicio1modulo6room.R
+import com.awakelab.oskar.ejercicio1modulo6room.databinding.FragmentAddBinding
+import com.awakelab.oskar.ejercicio2modulo6.data.local.Item
 
 class AddFragment : Fragment() {
-
+    lateinit var binding: FragmentAddBinding
+    private val itemViewModel: ItemViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        binding = FragmentAddBinding.inflate(layoutInflater, container, false)
+        initListener()
+        return binding.root
     }
+
+    private fun initListener() {
+        binding.btnGuardar.setOnClickListener {
+            val nombre = binding.editTextNombre.text.toString()
+            val precio = binding.editTextPrecio.text.toString().toInt()
+            val cantidad = binding.editTextCantidad.text.toString().toInt()
+
+            itemViewModel.insertItem(nombre,precio,cantidad)
+        }
+    }
+
 
 }
